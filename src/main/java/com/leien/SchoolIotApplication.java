7902f6801f97139f041220039873e3fee0d37c67
sishufeng <sishufeng0530@sina.com>
2019-09-16 19:36:51 +0800

    初始提交

---
+++ src/main/java/com/leien/SchoolIotApplication.java
@@ -0,0 +1,13 @@
+package com.leien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolIotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolIotApplication.class, args);
+    }
+
+}
