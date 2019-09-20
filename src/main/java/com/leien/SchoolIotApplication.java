b746801e51551375bb118ff01b26d746c329f937
sishufeng <sishufeng0530@sina.com>
2019-09-20 16:44:04 +0800

    添加扫描mapper的注解，用于全局扫描数据映射类

--- src/main/java/com/leien/SchoolIotApplication.java
+++ src/main/java/com/leien/SchoolIotApplication.java
@@ -1,9 +1,11 @@
 package com.leien;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.leien.dao")
 public class SchoolIotApplication {
 
     public static void main(String[] args) {
