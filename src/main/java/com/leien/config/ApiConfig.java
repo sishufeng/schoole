8774a97310358995f115c3dad78a9ec5d86db304
sishufeng <sishufeng0530@sina.com>
2019-10-09 11:12:43 +0800

    调用设备远程接口配置类

--- src/main/java/com/leien/config/ApiConfig.java
+++ src/main/java/com/leien/config/ApiConfig.java
@@ -1,6 +1,8 @@
 package com.leien.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -9,67 +11,18 @@ import org.springframework.stereotype.Component;
  * @Compny:LeiEnChuanMei
  * @Auther: SSF
  * @Date: 2019/9/29 12:02
- * @Description:调用设备远程接口配置类
+ * @Description:调用设备远程接口配置类，从配置文件读取数据
  */
 @Configuration
 @ConfigurationProperties(prefix = "api")
-@PropertySource("classpath:config/api.properties")
+@EnableConfigurationProperties
+@PropertySource("application.properties")
 @Component
+@Data
 public class ApiConfig {
     private String tokenUrl;
     private String tokenPathName;
-    private String token_key;
+    private String tokenKey;
     private String dataUrl;
     private String dataPathName;
-
-    public String getTokenUrl() {
-        return tokenUrl;
-    }
-
-    public void setTokenUrl(String tokenUrl) {
-        this.tokenUrl = tokenUrl;
-    }
-
-    public String getTokenPathName() {
-        return tokenPathName;
-    }
-
-    public void setTokenPathName(String tokenPathName) {
-        this.tokenPathName = tokenPathName;
-    }
-
-    public String getToken_key() {
-        return token_key;
-    }
-
-    public void setToken_key(String token_key) {
-        this.token_key = token_key;
-    }
-
-    public String getDataUrl() {
-        return dataUrl;
-    }
-
-    public void setDataUrl(String dataUrl) {
-        this.dataUrl = dataUrl;
-    }
-
-    public String getDataPathName() {
-        return dataPathName;
-    }
-
-    public void setDataPathName(String dataPathName) {
-        this.dataPathName = dataPathName;
-    }
-
-    @Override
-    public String toString() {
-        return "ApiConfig{" +
-                "tokenUrl='" + tokenUrl + '\'' +
-                ", tokenPathName='" + tokenPathName + '\'' +
-                ", token_key='" + token_key + '\'' +
-                ", dataUrl='" + dataUrl + '\'' +
-                ", dataPathName='" + dataPathName + '\'' +
-                '}';
-    }
 }
