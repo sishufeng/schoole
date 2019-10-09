dab5c33e63ada11b2cdbb3659c5ff8bce4233f56
sishufeng <sishufeng0530@sina.com>
2019-10-09 11:17:10 +0800

    定时访问远程接口获取设备信息数据

--- src/main/java/com/leien/SchoolIotApplication.java
+++ src/main/java/com/leien/SchoolIotApplication.java
@@ -1,8 +1,14 @@
 package com.leien;
 
+import com.alibaba.fastjson.JSONObject;
+import com.leien.utils.APIUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 @MapperScan("com.leien.dao")
@@ -10,6 +16,16 @@ public class SchoolIotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolIotApplication.class, args);
-    }
+        APIUtil api = new APIUtil();
+        String token = api.getToken();
 
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                String apiData = api.getData(token);
+                System.out.println(">>>>>>"+apiData);
+            }
+        },300,3000);
+    }
 }
