957ab007693069d368ff82575531b38ef5f67367
sishufeng <sishufeng0530@sina.com>
2019-10-17 14:49:01 +0800

    项目启动访问设备接口

--- src/main/java/com/leien/applicationrunner/AppRunner.java
+++ src/main/java/com/leien/applicationrunner/AppRunner.java
@@ -1,5 +1,8 @@
 package com.leien.applicationrunner;
 
+import com.leien.dao.DeviceDao;
+import com.leien.entity.Device;
+import com.leien.service.DeviceService;
 import com.leien.utils.APIUtil;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
@@ -25,14 +29,16 @@ import java.util.concurrent.TimeUnit;
 @Order(value = 1)
 public class AppRunner implements ApplicationRunner {
 
-    int count = 0;
     public List pageList = new ArrayList();
     public List dataList = new ArrayList();
     @Autowired
     APIUtil apiUtil;
+    @Autowired
+    private DeviceService deviceService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        List<Device> list = new ArrayList<>();
         String token = apiUtil.getToken();
 
         ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1,
@@ -44,12 +50,15 @@ public class AppRunner implements ApplicationRunner {
                 if (!StringUtils.isEmpty(token)){
                     String returnData = apiUtil.getData(token);
                     pageList.add(returnData);
-                    count++;
-                    if (count == 60){//3分钟往数据库保存一次
-                        dataList.add(returnData);
-                        System.out.println(">>>>>>>>>>>>++++++++>>>>>>"+returnData);
-                        count = 0;
+                    if(pageList.size()==50){
+                        deviceService.bulkInsertDevice(pageList);
                     }
+//                    count++;
+////                    if (count == 60){//3分钟往数据库保存一次
+////                        dataList.add(returnData);
+////                        System.out.println(">>>>>>>>>>>>++++++++>>>>>>"+returnData);
+////                        count = 0;
+////                    }
                 }
             }
         },3000,3000, TimeUnit.MILLISECONDS);
