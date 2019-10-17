a1ebc4d7236641ea27e18cccf10c1648c34919c3
sishufeng <sishufeng0530@sina.com>
2019-10-17 13:55:00 +0800

    新增业务字段

--- src/main/java/com/leien/entity/Device.java
+++ src/main/java/com/leien/entity/Device.java
@@ -13,36 +13,74 @@ import java.util.Date;
  */
 @Data
 public class Device {
+
+
     private String id;
+
     /**
-     * 设备类型ID
+     * 设备类型(0=温控器，1=风机)
      */
-    private Integer deviceTypeId;
+    private Integer deviceType;
+
+    /**
+     * 阀门状态(0：关，1：开)
+     */
+    private Integer valveStatus;
+
+    /**
+     * 设备状态(0：关，1：开)
+     */
+    private Integer deviceStatus;
+
+    /**
+     * 进水温度
+     */
+    private double  inletTemperature;
+
+    /**
+     * 回水温度
+     */
+    private double  reWaterTemperature;
+
+    /**
+     * 设备湿度
+     */
+    private double  deviceHumidity;
+
+    /**
+     * 备一温度
+     */
+    private double  beiYiTemperature;
+
+    /**
+     * 备二温度
+     */
+    private double  beiErTemperature;
 
     /**
      * 设备创建时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
+
     /**
-     * 设备名称
+     *  设备名称
      */
-    private String deviceName;
+    private String  deviceName;
+
     /**
      * 设备说明
      */
     private String deviceRemark;
-    /**
-     * 设备编号
-     */
-    private Integer deviceNumber;
-    /**
-     * 设备状态
-     */
-    private Integer deviceStatus;
+
     /**
      * 访问接口使用
      */
-    private String uuid;
+    private String deviceUUID;
+
+    /**
+     * 设备所属学校
+     */
+    private String affiliatedSchool;
 
 }
