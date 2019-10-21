b53855356e6b28b565021bed1cc70252d170844a
sishufeng <sishufeng0530@sina.com>
2019-10-21 17:36:16 +0800

    提交

--- src/main/java/com/leien/entity/DeviceReturnData.java
+++ src/main/java/com/leien/entity/DeviceReturnData.java
@@ -15,7 +15,7 @@ public class DeviceReturnData {
     /**
      * 设备类型(0=温控器，1=风机)
      */
-    private Integer deviceType;
+    private String deviceType;
     /**
      * 进水温度
      */
@@ -27,7 +27,7 @@ public class DeviceReturnData {
     /**
      * 阀门状态(0：关，1：开)
      */
-    private Integer valveStatus;
+    private String valveStatus;
 
     /**
      * 备一温度
@@ -38,10 +38,6 @@ public class DeviceReturnData {
      */
     private double  beiErTemperature;
     /**
-     * 设备类型名称
-     */
-    private String typeName;
-    /**
      * 添加时间
      */
     private String addTime;
@@ -49,4 +45,25 @@ public class DeviceReturnData {
      * 设备UUID
      */
     private String deviceUuid;
+
+    /**
+     * 设备名称
+     */
+    private String name;
+
+    /**
+     * 设备状态(0：在线，1：离线)
+     */
+    private Integer zhaungtai;
+
+    /**
+     * 设备状态名称
+     */
+    private String zhuangtaiName;
+
+    /**
+     * 风机状态(1：开，0：关)
+     */
+    private String fanStatus;
+
 }
